320ba214bcce277a532a4c4df93b5efd45a75301
fullbearded <runner.dada@gmail.com>
2023-04-20 13:07:40 +0800

    feat(website): change the home page app list

--- websites/src/pages/Website/Home/Feature7.jsx
+++ websites/src/pages/Website/Home/Feature7.jsx
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Row, Col, Button } from 'antd';
 import { PlayCircleOutlined, LikeOutlined } from '@ant-design/icons';
 
 import { getChildrenToRender } from './utils';
+import {likeFreeApp, pageFreeApp} from "@/services/server/api";
 
 function Feature7(props) {
-  const { dataSource, isMobile, ...tagProps } = props;
-  const { blockWrapper, titleWrapper } = dataSource;
-  const childrenToRender = blockWrapper.children.map((item, i) => (
-    <Col {...item} key={i.toString()}>
-      <div {...item.children}>
-        <div className="summary">{item.children.children.map(getChildrenToRender)}</div>
+  const perPage = 16;
+
+  const [appPageData, setAppPageData] = useState({page: 1, perPage: 20, content: [], totalPages: 1, total: 100});
+  const [like, setLike] = useState({});
+  const [showSection, setShowSection] = useState(true);
+
+  useEffect(() => {
+    pageFreeApp({recommend: "HOME", perPage: perPage}).then((res) => {
+      debugger
+      if (res.data.content.length === 0) {
+        setShowSection(false);
+      }
+      setAppPageData(res.data);
+    });
+  }, {});
+
+  const handleLikeApp = (value) => {
+    likeFreeApp({code: value}).then(r => {
+      let timeout;
+      setLike({...like, [value]: true});
+      timeout = setTimeout(() => {
+        setLike({...like, [value]: false});
+      }, 800);
+      return () => {
+        clearTimeout(timeout);
+      }
+    });
+  }
+
+  const childrenToRender = appPageData.content.map((item, i) => (
+    <Col md={6} xs={24} className="feature7-block" key={item.code}>
+      <div className="feature7-block-group">
+        <div className="summary">
+          <div className="feature7-block-image">{item.icon}</div>
+          <h1 className="feature7-block-title">{item.name}</h1>
+          <div className="feature7-block-content">{item.description}</div>
+        </div>
         <div className="scenes">
           <Button
             type=""
-            className="like"
-            icon={<LikeOutlined />}
+            className={like[item.code] ? "like-success like" : "like"}
+            icon={<LikeOutlined/>}
             size="middle"
             loading={false}
             block={false}
+            onClick={() => {handleLikeApp(item.code)}}
           >
             点赞
           </Button>
           <Button
             type=""
             className="run"
-            icon={<PlayCircleOutlined />}
+            icon={<PlayCircleOutlined/>}
             size="middle"
             loading={false}
             block={false}
-            target="_blank"
-            href="/app/xxxx"
+            href={`/app/${item.code}`}
           >
             运行
           </Button>
@@ -38,18 +70,21 @@ function Feature7(props) {
       </div>
     </Col>
   ));
+
+  const { dataSource, isMobile, ...tagProps } = props;
+  const { blockWrapper, titleWrapper } = dataSource;
   return (
-    <div {...tagProps} {...dataSource.wrapper}>
-      <div {...dataSource.page}>
-        <div {...dataSource.titleWrapper}>{titleWrapper.children.map(getChildrenToRender)}</div>
-        <div>
-          <Row {...blockWrapper}>{childrenToRender}</Row>
-        </div>
-        <div className="more-app">
-          <Button type="primary" shape="round" className="more-app-button" href="/apps">查看更多</Button>
+      <div {...tagProps} {...dataSource.wrapper} style={showSection ? {} : {display: 'none'}}>
+        <div {...dataSource.page}>
+          <div {...dataSource.titleWrapper}>{titleWrapper.children.map(getChildrenToRender)}</div>
+          <div>
+            <Row className="feature7-block-wrapper" gutter={24}>{childrenToRender}</Row>
+          </div>
+          <div className="more-app">
+            <Button type="primary" shape="round" className="more-app-button" href="/apps">查看更多</Button>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
